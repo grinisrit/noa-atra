975a4cc927096d5bb4bf224ce54fd5504df7eafe
Andrei Kislitsyn <kisandy@yandex.ru>
2021-07-03 14:06:04 +0300

    fix reconnect

--- crypto-hft-data/src/main/kotlin/com/grinisrit/crypto/CoinBaseThread.kt
+++ crypto-hft-data/src/main/kotlin/com/grinisrit/crypto/CoinBaseThread.kt
@@ -4,14 +4,17 @@ import io.ktor.client.*
 import io.ktor.client.engine.java.*
 import io.ktor.client.features.websocket.*
 import io.ktor.http.cio.websocket.*
+import kotlinx.coroutines.*
 import kotlinx.coroutines.flow.collect
 import kotlinx.coroutines.flow.flow
-import kotlinx.coroutines.*
 import org.zeromq.SocketType
 import org.zeromq.ZContext
+import java.io.BufferedOutputStream
 import java.io.File
-import java.lang.Exception
+import java.io.FileOutputStream
+import java.io.PrintStream
 import java.util.*
+
 
 const val incomingCheckDelay = 2000L
 const val reconnectDelay = 5000L
@@ -26,6 +29,8 @@ class CoinBaseThread(private val coinbase: Coinbase, zeroMQ: ZeroMQ) : Thread() 
 
     private val zeroMQAddress = "tcp://${zeroMQ.address}:${zeroMQ.port}"
 
+    private val loggerFile = PrintStream(BufferedOutputStream(FileOutputStream("log.txt")), true)
+
     override fun run() {
         val context = ZContext()
         val socket = context.createSocket(SocketType.PUB)
@@ -34,7 +39,8 @@ class CoinBaseThread(private val coinbase: Coinbase, zeroMQ: ZeroMQ) : Thread() 
         runBlocking {
             while (true) {
                 try {
-                    println("Trying to connect...")
+                    loggerFile.println("Trying to connect...")
+                    loggerFile.println(calendar.time)
 
                     val currentTime = calendar.timeInMillis
 
@@ -47,7 +53,8 @@ class CoinBaseThread(private val coinbase: Coinbase, zeroMQ: ZeroMQ) : Thread() 
                         socket.send(it)
                     }
                 } catch (e: Throwable) {
-                    println("Catch $e")
+                    loggerFile.println("Catch $e")
+                    loggerFile.println(calendar.time)
                     delay(reconnectDelay)
                 }
 
@@ -67,20 +74,37 @@ class CoinBaseThread(private val coinbase: Coinbase, zeroMQ: ZeroMQ) : Thread() 
 
 
         client.wss(host = coinbase.address) {
-            println("Connected successfully")
-            println(calendar.time)
+            loggerFile.println("Connected successfully")
+            loggerFile.println(calendar.time)
             send(Frame.Text(request))
             val subResponse = incoming.receive()
             subResponse as? Frame.Text ?: throw Exception("Bad response")
-            println("Request sent. Server response:")
-            println(subResponse.readText())
+            loggerFile.println("Request sent. Server response:")
+            loggerFile.println(subResponse.readText())
 
+            var gotAnswer = true
+
+            val timer = launch {
+                while (true) {
+                    gotAnswer = false
+                    delay(incomingCheckDelay)
+                    if (!gotAnswer) {
+                        this@wss.cancel()
+                    }
+                }
+            }
 
             for (frame in incoming) {
+                gotAnswer = true
                 frame as? Frame.Text ?: continue
                 val dateTime = cbFormat(calendar.time)
                 emit("${frame.readText()}///<>///$dateTime")
             }
+
+
+
+
+
 
 
         }
