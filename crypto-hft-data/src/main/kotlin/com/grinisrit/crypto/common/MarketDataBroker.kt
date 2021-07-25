f112198d232af0b12d2bffb59ef6da93989972ec
Roland Grinis <roland.grinis@grinisrit.com>
2021-07-25 20:55:52 +0100

    refactored market broker

--- crypto-hft-data/src/main/kotlin/com/grinisrit/crypto/common/MarketDataBroker.kt
+++ crypto-hft-data/src/main/kotlin/com/grinisrit/crypto/common/MarketDataBroker.kt
@@ -2,7 +2,6 @@ package com.grinisrit.crypto.common
 
 import com.grinisrit.crypto.ConfYAMl
 import com.grinisrit.crypto.ZeroMQConfig
-import com.grinisrit.crypto.logger
 
 import kotlinx.coroutines.CoroutineScope
 import kotlinx.coroutines.Dispatchers
@@ -21,79 +20,67 @@ typealias MarkedDataFlow = SharedFlow<MarkedData>
 typealias MutableRawMarketDataFlow = MutableSharedFlow<RawMarketData>
 typealias RawMarketDataFlow = Flow<RawMarketData>
 
-fun CoroutineScope.createMarketDataBroker(conf: ConfYAMl): MarketDataBroker {
-    val launchPub = with(conf.platforms) {
-        coinbase.isOn or binance.isOn or bitstamp.isOn or kraken.isOn or deribit.isOn
-    }
-    val launchSub = conf.mongodb.isOn or conf.platforms.binance.isOn
-    return MarketDataBroker(this, conf.zeromq, launchPub, launchSub)
-}
+fun CoroutineScope.createMarketDataBroker(conf: ConfYAMl): MarketDataBroker =
+    MarketDataBroker.fromConfig(this, conf)
 
 
-class MarketDataBroker internal constructor(
+class MarketDataBroker private constructor(
     private val coroutineScope: CoroutineScope,
-    private val zmqConfig: ZeroMQConfig,
-    private val launchPub: Boolean,
-    private val launchSub: Boolean
+    private val pubService: MarketDataPubService?,
+    private val subService: MarketDataSubService?
 ) {
 
-    private val inFlow: MutableRawMarketDataFlow = MutableSharedFlow()
-    private val outFlow: MutableMarkedDataFlow = MutableSharedFlow()
+    companion object {
+        internal fun fromConfig(scope: CoroutineScope, conf: ConfYAMl): MarketDataBroker =
+            MarketDataBroker(
+                scope,
+                with(conf.platforms) {
+                    if (coinbase.isOn or binance.isOn or bitstamp.isOn or kraken.isOn or deribit.isOn)
+                        MarketDataPubService(conf.zeromq) else null
+                },
+                if (conf.mongodb.isOn or conf.platforms.binance.isOn)
+                    MarketDataSubService(conf.zeromq) else null
+            )
+    }
 
-    fun getFlow(): MarkedDataFlow? =
-        if (launchSub) {
-            outFlow.asSharedFlow()
-        } else {
-            logger.warn { "No subscription for market data launched on ${zmqConfig.address}" }
-            null
-        }
+    fun getFlow(): MarkedDataFlow? = subService?.getFlow()
 
-    suspend fun publishFlow(rawMarketDataFlow: RawMarketDataFlow) =
-        if (launchPub) {
-            rawMarketDataFlow.collect { inFlow.emit(it) }
-        } else {
-            logger.warn { "No market data publication server launched on ${zmqConfig.address}" }
-        }
+    suspend fun publishFlow(rawMarketDataFlow: RawMarketDataFlow): Unit? =
+        pubService?.publishFlow(rawMarketDataFlow)
 
     fun launchBroker(): Job =
         coroutineScope.launch(Dispatchers.IO) {
-
             val logger = KotlinLogging.logger { }
-
             ZContext().use { context ->
-
-                if (launchPub) {
-                    val pubSocket = context.getPubSocket(zmqConfig, logger)
-                    inFlow.onEach { rawData ->
-                        pubSocket.send(rawData)
-                    }.launchIn(this)
-                }
-
-                if (launchSub) {
-                    val subSocket = context.getSubSocket(zmqConfig, logger)
-                    for (rawData in subSocket.recvStrStream())
-                        try {
-                            outFlow.emit(MarketDataParser.parseRawMarketData(rawData))
-                        } catch (e: Throwable) {
-                            logger.error(e) { "Received corrupted market data" }
-                        }
-                }
+                pubService?.launchPubService(context, logger)?.launchIn(this)
+                subService?.launchSubService(context, logger)
             }
         }
+}
 
-    private val pubInfo = "Publishing market data on ${zmqConfig.address}"
-    private val pubError = "Failed to launch market data publication on ${zmqConfig.address}"
+
+// None of those classes shall appear anywhere else
+
+private sealed class ZMQService {
+    protected var socket: ZMQ.Socket? = null
+}
+
+private class MarketDataSubService(val zmqConfig: ZeroMQConfig) : ZMQService() {
+    private val outFlow: MutableMarkedDataFlow = MutableSharedFlow()
     private val subInfo = "Subscribing to market data feed on ${zmqConfig.address}"
     private val subError = "Failed to consume market data from ${zmqConfig.address}"
 
-    private fun ZContext.getPubSocket(zmq: ZeroMQConfig, logger: KLogger): ZMQ.Socket =
-        try {
-            logger.debug { pubInfo }
-            createSocket(SocketType.PUB).apply { bind(zmq.address) }
-        } catch (e: Throwable) {
-            logger.error(e) { pubError }
-            throw RuntimeException(pubError)
-        }
+    fun getFlow(): MarkedDataFlow = outFlow.asSharedFlow()
+
+    suspend fun launchSubService(context: ZContext, logger: KLogger) {
+        val socket = context.getSubSocket(zmqConfig, logger)
+        for (rawData in socket.recvStrStream())
+            try {
+                outFlow.emit(MarketDataParser.parseRawMarketData(rawData))
+            } catch (e: Throwable) {
+                logger.error(e) { "Received corrupted market data" }
+            }
+    }
 
     private fun ZContext.getSubSocket(zmq: ZeroMQConfig, logger: KLogger): ZMQ.Socket =
         try {
@@ -106,5 +93,30 @@ class MarketDataBroker internal constructor(
             logger.error(e) { subError }
             throw RuntimeException(subError)
         }
-
 }
+
+private class MarketDataPubService(val zmqConfig: ZeroMQConfig) : ZMQService() {
+    private val inFlow: MutableRawMarketDataFlow = MutableSharedFlow()
+    private val pubInfo = "Publishing market data on ${zmqConfig.address}"
+    private val pubError = "Failed to launch market data publication on ${zmqConfig.address}"
+
+    suspend fun publishFlow(rawMarketDataFlow: RawMarketDataFlow) =
+        rawMarketDataFlow.collect { inFlow.emit(it) }
+
+    fun launchPubService(context: ZContext, logger: KLogger): Flow<RawMarketData> {
+        socket = context.getPubSocket(zmqConfig, logger)
+        return inFlow.onEach { rawData ->
+            socket?.send(rawData)
+        }
+    }
+
+    private fun ZContext.getPubSocket(zmq: ZeroMQConfig, logger: KLogger): ZMQ.Socket =
+        try {
+            logger.debug { pubInfo }
+            createSocket(SocketType.PUB).apply { bind(zmq.address) }
+        } catch (e: Throwable) {
+            logger.error(e) { pubError }
+            throw RuntimeException(pubError)
+        }
+}
+
