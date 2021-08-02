dc9e7431048e5323d7d209febace1c94d8358188
Roland Grinis <roland.grinis@grinisrit.com>
2021-08-02 15:29:38 +0100

    little refactor to last commit

--- crypto-hft-visual/src/main/kotlin/com/grinisrit/crypto/bitstamp/tradeVolume.kt
+++ crypto-hft-visual/src/main/kotlin/com/grinisrit/crypto/bitstamp/tradeVolume.kt
@@ -1,19 +1,11 @@
-package com.grinisrit.crypto
+package com.grinisrit.crypto.bitstamp
 
-import com.grinisrit.crypto.bitstamp.*
 import org.litote.kmongo.KMongo
 import org.litote.kmongo.div
 import org.litote.kmongo.eq
 import org.litote.kmongo.getCollection
 import space.kscience.plotly.*
 import space.kscience.plotly.models.ScatterMode
-import java.time.Instant
-
-
-data class TimestampedMarketTrade(
-    val receiving_datetime: Instant,
-    val platform_data: Trade,
-)
 
 fun getTotalAmount(data: List<OrderData>): Float {
     return data.map { it.amount }.sum()
@@ -25,9 +17,9 @@ fun OrderBook.getTotalAmounts(): Pair<Float, Float> {
 
 fun loadAllTrades(): List<Trade> {
     val mongo = KMongo.createClient("mongodb://localhost:27017")
-    val col = mongo.getDatabase("bitstamp").getCollection<TimestampedMarketTrade>("trade")
+    val col = mongo.getDatabase("bitstamp").getCollection<TimestampedTrade>("trade")
 
-    val res = col.find(TimestampedMarketTrade::platform_data / Trade::channel eq "live_trades_btcusd")
+    val res = col.find(TimestampedTrade::platform_data / Trade::channel eq "live_trades_btcusd")
 
     return res.toList().map { it.platform_data }
 
